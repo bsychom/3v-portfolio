8c44610f4130913580725b750be2ea2f9acb2da5
mhaddaou <medo19981602@gmail.com>
2024-09-09 19:17:56 +0100

    up

--- components/sub/LayoutGridDemo.tsx
+++ components/sub/LayoutGridDemo.tsx
@@ -1,7 +1,9 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
 import { LayoutGrid } from "../ui/layout-grid";
-
+import Image from "next/image";
+import {motion} from 'framer-motion'
+import Link from "next/link";
 export function LayoutGridDemo() {
   return (
     <div className="min-h-screen  py-20 w-full relative">
@@ -33,12 +35,33 @@ const SkeletonTwo = () => {
       <p className="font-bold md:text-4xl text-xl text-white font-lemonada">
       Financial App
         </p>
-      <p className="font-medium text-base w-[70%] pt-5 font-lexend-deca text-[#B7AEAE]">
+      <p className="font-medium  w-[70%] pt-5 font-lexend-deca text-[#B7AEAE] text-sm">
 This project is a sleek and user-friendly financial app built entirely on the front end. Leveraging the power of Next.js 13, Tailwind CSS, TypeScript, and DeasyUI, this app delivers a seamless financial management experience .     
 
       </p>
-        <div  className="text-white w-full flex ">
-          <button>first</button><button>second</button>
+        <div className="flex justify-end pr-12 gap-5 py-9">
+        <motion.button className="shadow-2xl shadow-white   rounded-lg"
+          whileHover={{scale: 1.1,transition:{duration: 1} }} whileTap={{ scale: 0.9 }}
+          >
+        <Link href="https://github.com/mhaddaou/App_Financial" target="_blank"
+          className="py-3.5 text-sm  relative button-primary gap-3  px-4 border-[0.1px] capitalize flex border-[#7042f8] font-lemonada font-bold tracking-wider text-center  shadow-2xl shadow-white text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          code base
+          <Image src='/git.svg' alt="" width={20} height={12}/>
+        </Link>
+
+          </motion.button>
+          <motion.button className="shadow-2xl shadow-white   rounded-xl"
+          whileHover={{scale: 1.1,transition:{duration: 1} }} whileTap={{ scale: 0.9 }}
+          >
+        <Link href="https://financial-gray.vercel.app/" target="_blank" 
+          className="py-3.5 relative button-primary px-4 border-[0.1px] capitalize border-[#7042f88b] font-lemonada font-bold tracking-wider  flex gap-3 text-center text-white cursor-pointer text-sm  rounded-lg max-w-[200px]"
+        >
+          quick view <Image src='/redirect.png' alt="" width={20} height={12}/>
+        </Link>
+
+          </motion.button>
+          
         </div>
     </div>
   );
